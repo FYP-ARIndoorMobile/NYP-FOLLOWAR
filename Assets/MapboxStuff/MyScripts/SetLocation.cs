089f2a911f261e0aad01534a86f58a6f06525b14
Fatin <fatin.atiqah.yusof@gmail.com>
2019-10-09 14:48:48 +0800

    Added in MapboxStuff

---
+++ Assets/MapboxStuff/MyScripts/SetLocation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Mapbox.Directions;
+using System.Collections.Generic;
+using System.Linq;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+using System.Collections;
+
+public class SetLocation : MonoBehaviour
+{
+    //bool _isInitialized;
+    [SerializeField]
+    AbstractMap _map;
+    [SerializeField]
+    Transform waypoint;
+    [SerializeField]
+    double lat;
+    [SerializeField]
+    double lng;
+
+    protected virtual void Awake()
+    {
+        if (_map == null)
+        {
+            _map = FindObjectOfType<AbstractMap>();
+        }
+    }
+
+    public void Update()
+    {
+        moveWaypointToGeoLocation();
+    }
+    void moveWaypointToGeoLocation()
+    {
+        waypoint.MoveToGeocoordinate(lat, lng, _map.CenterMercator, _map.WorldRelativeScale);
+    }
+}
+
+
+
