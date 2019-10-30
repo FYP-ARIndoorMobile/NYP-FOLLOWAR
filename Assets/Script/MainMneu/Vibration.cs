7cd45cb9c0855511b5c211f982990b8a3896e093
Fatin <fatin.atiqah.yusof@gmail.com>
2019-10-30 16:22:19 +0800

    Singleton VIbration Script

---
+++ Assets/Script/MainMneu/Vibration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vibration : MonoBehaviour
+{
+    public static Vibration _instance;
+    public Toggle toggle;
+
+    public static Vibration Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<Vibration>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void VibrateToggle()
+    {
+        if (toggle.isOn == true)
+        {
+            Vibrate();
+        }
+    }
+    private void Vibrate()
+    {
+        print("Vibrate Start");
+        StartCoroutine("VibrateDuration", 1.0f);
+    }
+    IEnumerator VibrateDuration(float waitTime)
+    {
+        Handheld.Vibrate();
+        yield return new WaitForSeconds(waitTime);
+        print("Vibrate Stop");
+    }
+
+
+
+}
