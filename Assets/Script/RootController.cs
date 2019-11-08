67d1e3bdfaf4899ba723e66882143cda46937e4a
Jordan Tan Yan Boon <31056129+174561B@users.noreply.github.com>
2019-11-08 16:58:27 +0800

    Walking and idle animation added

--- Assets/Script/RootController.cs
+++ Assets/Script/RootController.cs
@@ -17,7 +17,9 @@ public class RootController : MonoBehaviour
     public float DistFromLastRoot = 1.0f;
 
     public Animator animationPanda;
-   
+
+    private float ElaspedTime;
+    private Vector3 PrevPos;
 
     // Start is called before the first frame update
     void Start()
@@ -26,14 +28,57 @@ public class RootController : MonoBehaviour
         // ガイドオブジェクトを固定する
         this.transform.position = this.transform.localPosition;
 
-      //  meshLine = TargetPos.GetComponent<meshLine>();
+        //  meshLine = TargetPos.GetComponent<meshLine>();
 
         InstantiateFlag = false;
-
+        ElaspedTime = 0;
     }
 
     void Update()
     {
+
+        ElaspedTime += Time.deltaTime;
+        if (ElaspedTime > 1.0f)
+        {
+            DebugUIManager.instance.DebugLog("START");
+            ElaspedTime = 0;
+            if (PrevPos == null)
+            {
+                PrevPos = gameObject.transform.position;
+            }
+            else
+            {
+                float distMoved = Vector3.Distance(PrevPos, gameObject.transform.position);
+                DebugUIManager.instance.DebugLog("Dist: " + distMoved.ToString());
+                if (distMoved < 1)
+                {
+                    DebugUIManager.instance.DebugLog("Set Idle");
+                    animationPanda.SetTrigger("SetIdle");
+                }
+                else
+                {
+                    DebugUIManager.instance.DebugLog("Set Walking");
+                    animationPanda.SetTrigger("SetWalking");
+                }
+            }
+            DebugUIManager.instance.DebugLog("Prev: " + PrevPos.ToString());
+            DebugUIManager.instance.DebugLog("Curr: " + gameObject.transform.position.ToString());
+            PrevPos = gameObject.transform.position;
+        }
+
+        /*
+            if (PrevPos != null)
+            {
+                float distMoved = Vector3.Distance(PrevPos, gameObject.transform.position);
+                DebugUIManager.instance.DebugLog(distMoved.ToString());
+                if (distMoved < 0.5f)
+                {
+                    DebugUIManager.instance.DebugLog("Set Idle");
+                    animationPanda.SetTrigger("SetIdle");
+                }
+            }
+        */
+
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         transform.LookAt(DebugUIManager.instance.FirstPersonCamera.transform.position);
         transform.Rotate(0, 180, 0);
@@ -42,7 +87,7 @@ public class RootController : MonoBehaviour
         //    this.transform.LookAt(meshLine.target);
 
         // オブジェクトが止まっているならフラグをfalseにする
-        if (rigid.IsSleeping()==true)
+        if (rigid.IsSleeping() == true)
         {
             InstantiateFlag = false;
         }
@@ -54,7 +99,7 @@ public class RootController : MonoBehaviour
             //    previousRootPrint = Instantiate(footPrintPrefab, transform.position, transform.rotation);
             //else if (Vector3.Distance(previousRootPrint.transform.position, transform.position) > DistFromLastRoot)
             //    previousRootPrint = Instantiate(footPrintPrefab, transform.position, transform.rotation);
-                
+
             if (previousRoot == null)
             {
                 newRoot = Instantiate(footPrintPrefab, transform.position, transform.rotation);
