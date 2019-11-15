f2d42fa127bc16c3b064694da6b1a13ba46a8121
Fatin <fatin.atiqah.yusof@gmail.com>
2019-11-15 17:45:33 +0800

    doggo and poi and route and idk

---
+++ Assets/Script/MainMenu/MainMenuoption.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuoption : MonoBehaviour
+{
+    public Canvas theoptionCanvas;
+    public GameObject optionLightDark;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (theoptionCanvas == null)
+            theoptionCanvas = GameObject.Find("Option Canvas").GetComponentInChildren<Canvas>();
+
+        if (optionLightDark == null)
+            optionLightDark = GameObject.FindGameObjectWithTag("Option");
+    }
+    public void OptionCanvasOn()
+    {
+        theoptionCanvas.gameObject.SetActive(true);
+    }
+    public void OptionCavasOff()
+    {
+        theoptionCanvas.gameObject.SetActive(false);
+    }
+
+    public void OptionLightDarkOn()
+    {
+        optionLightDark.gameObject.SetActive(true);
+    }
+
+    public void OptionLightDarkOff()
+    {
+        optionLightDark.gameObject.SetActive(false);
+    }
+
+}
