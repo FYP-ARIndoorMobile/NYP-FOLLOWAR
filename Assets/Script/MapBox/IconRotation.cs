6f014e0458a5a4455a7d722667655b26658be09f
Fatin <fatin.atiqah.yusof@gmail.com>
2019-11-19 17:46:14 +0800

    Readjust some UI. Update on help page.

---
+++ Assets/Script/MapBox/IconRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    [SerializeField]
+    float height = 2f;
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, height, transform.localPosition.z);
+        transform.Rotate(Vector3.up * Time.deltaTime * 20);
+    }
+}
